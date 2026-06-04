0379648bda28d1b1d90f144c5fae600b771919ad
LeadingTheAbyss <kartikeykumarsingh27jun@gmail.com>
2026-06-04 16:00:37 +0530

    Fix unused React import error in Y2KHero

--- src/components/Y2KHero.tsx
+++ src/components/Y2KHero.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import heroImage from '../images/bg.png';
